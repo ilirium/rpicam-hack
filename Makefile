e98f99e562c43d2690d6e9404616898562cfffd8
Ilya 'Ilirium' Kalimulin <ilirium115@gmail.com>
2025-04-30 13:22:53 +0300

    added: save metadata as txt and pass metadata to save in EXIF

---
+++ Makefile
@@ -0,0 +1,5 @@
+cam:
+	DISPLAY=:0 rpicam-hello -t 0 -f --hdr=auto
+
+mycam:
+	./run_experiment.sh
--- experiments/series001/ex001_first_try.py
+++ experiments/series001/ex001_first_try.py
@@ -6,6 +6,14 @@ from picamera2 import Picamera2, Preview
 
 from experiments.series001 import PATH_PKG
 
+"""
+picam2 = picamera2.Picamera2
+picam2.helpers.save
+
+picamera2/request.py/Helpers/save
+
+"""
+
 
 def print_metadata(metadata):
     for key, value in metadata.items():
@@ -48,16 +56,23 @@ def snap2(path_dir_out: Path):
     # Step: Save
     for i in range(3):
         dt = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S%f")
-        path_file_out = path_dir_out / f'DSCPI_{dt}.jpg'
+        print(f'snap {i}, {dt}')
+
+        path_file_out_img = path_dir_out / f'DSCPI_{dt}.jpg'
+        path_file_out_txt = path_dir_out / f'DSCPI_{dt}.txt'
 
         (buffer,), metadata = picam2.capture_buffers(["main"])
-        print(dt)
-        print_metadata(metadata)
-        print()
+
         img = picam2.helpers.make_image(buffer, picam2.camera_configuration()["main"])
-        picam2.helpers.save(img, metadata, path_file_out)
+        picam2.helpers.save(img=img, metadata=metadata, file_output=str(path_file_out_img))
+
+        with open(path_file_out_txt, 'w') as fp:
+            for key, value in metadata.items():
+                fp.write(f'{key}: {value}\n')
 
 
 if __name__ == "__main__":
-    path_dir_out = PATH_PKG / "out"
+    folder_dt_day = datetime.now(timezone.utc).strftime("%Y%m%d")
+    folder_dt_full = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
+    path_dir_out = PATH_PKG / "out" / folder_dt_day / folder_dt_full
     snap2(path_dir_out)
